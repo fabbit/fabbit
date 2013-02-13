fcd7ca0c2f0f51a18f145cceec69d90065c7003e
kohta <keienohta@gmail.com>
2013-02-12 16:33:01 -0800

    Initialized Rails application

---
+++ fabbit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabbit::Application.config.secret_token = 'a78423447a77502bf1f483e4b67c6b3567abd4132ebc4e2f4936164948b49baa15ae79160b07c7295d2abadf238086af81db6a4b9d58859ab22640ffde778adb'
