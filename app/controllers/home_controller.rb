d54945c00b0bb76f4764cea53d56208ab44ec15c
Keien Ohta <keien.ohta@gmail.com>
2013-07-21 16:35:10 -0700

    Fixed bug with home page and breadcrumbs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
 
   # Renders home page or redirects to root folder if already logged in
   def home
-    redirect_to navigate_path if cookies[:dropbox_session] and dropbox_session.authorized? and current_member?
+    # redirect_to navigate_path if cookies[:dropbox_session] and dropbox_session.authorized? and current_member?
   end
 
   def help
