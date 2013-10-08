89caad97484052791f90e9629c03ff9de15b62dd
Keien Ohta <keienohta@gmail.com>
2013-10-08 05:30:27 -0700

    Fix for app not deploying

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Fabbit
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # some heroku line
+    config.assets.initialize_on_precompile = false
   end
 end
