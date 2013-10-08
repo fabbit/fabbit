782380a3f3382ed95144dbc4f2ba25d47637ef82
bhaviksingh <bhaviksingh@gmail.com>
2013-10-07 17:54:19 -0700

    member viewing for projects

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,18 +2,14 @@ class MembersController < ApplicationController
 
   def show
 
-    if current_member.admin?
-      if params[:project_id]
-        @project = Project.find(params[:project_id])
-        @member = Member.find(params[:id])
-        @model_files = @member.files_in(@project)
+    if params[:project_id]
+      @project = Project.find(params[:project_id])
+      @member = Member.find(params[:id])
+      @model_files = @member.files_in(@project)
 
-        Breadcrumbs.add title: "Projects", link: navigate_url
-        Breadcrumbs.add title: @project.name, link: @project
-        Breadcrumbs.add title: @member.name, link: project_member_url(@project, @member)
-      end
-    else
-      redirect_to root_path
+      Breadcrumbs.add title: "Projects", link: navigate_url
+      Breadcrumbs.add title: @project.name, link: @project
+      Breadcrumbs.add title: @member.name, link: project_member_url(@project, @member)
     end
 
   end
